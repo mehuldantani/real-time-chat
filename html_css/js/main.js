c4aa89954271d7cb2e5c8f1a3099220214e537d3
Mehul Dantani <63199571+mehuldantani@users.noreply.github.com>
2020-05-31 19:35:03 +0530

    updated file with latestchanges

--- html_css/js/main.js
+++ html_css/js/main.js
@@ -1,21 +1,50 @@
-const signUpbtn = document.getElementById('signup');
-const signInbtn = document.getElementById('signin');
-const container = document.getElementById('container');
-const forgot = document.getElementById('forgot-pw');
-const back = document.getElementById('back');
+const chatField = document.getElementById('chat-message-field');
+const scrollset = document.querySelector('.message');
+const socket = io();
 
-signUpbtn.addEventListener('click',()=>{
-	container.classList.add('right-panel-active')
-});
+//send message to chat screen
+socket.on('message', message=>{
+	outputMessage(message);
 
-signInbtn.addEventListener('click',()=>{
-	container.classList.remove('right-panel-active')
-});
+	//scroll set to bottom after seding a new message
+	scrollset.scrollTop = scrollset.scrollHeight;
 
-forgot.addEventListener('click',()=>{
-	container.classList.add('forgot-panel-active')
-});
+})
 
-back.addEventListener('click',()=>{
-	container.classList.remove('forgot-panel-active')
-});
+//send-message
+
+chatField.addEventListener('submit',(e)=>{
+e.preventDefault();
+
+
+//fetch text message details from container
+const msg = e.target.elements.msg.value;
+
+//send this text to server
+socket.emit('chat-msg',msg);
+yourMessage(msg);
+scrollset.scrollTop = scrollset.scrollHeight;
+
+//clear text feild after sendig a message
+e.target.elements.msg.value ='';
+e.target.elements.msg.focus();
+
+
+})
+
+function outputMessage(message){
+const div = document.createElement('div');
+div.classList.add('message');
+div.innerHTML =`<p class="meta">${message.username}<span>${message.time}</span></p>
+<p class="text">${message.text}</p>`
+document.querySelector('.chat-messages').appendChild(div);
+};
+
+function yourMessage(message){
+	const div = document.createElement('div');
+	div.classList.add('message');
+	div.classList.add('send');
+	div.innerHTML =`<p class="meta">Mehul<span>10.20</span></p>
+	<p class="text">${message}</p>`
+	document.querySelector('.chat-messages').appendChild(div);
+	};
