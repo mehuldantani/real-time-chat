2980383758369817ec3aa6d75e5cfa8cff309e4a
Mehul Dantani <63199571+mehuldantani@users.noreply.github.com>
2020-06-07 14:58:59 +0530

    With latest changes

--- others/messages.js
+++ others/messages.js
@@ -6,6 +6,16 @@ function messageStructure(username, text) {
     text,
     time: moment().format('h:mm a')
     }
-}
+};
+
+function selfMessage(username, text) {
+    return {
+    username,
+    text,
+    time: moment().format('h:mm a')
+    }
+};
+
 
 module.exports = messageStructure;
+module.exports = selfMessage;
